3f85e3d0922372e4d0bde2c801c7e4683d3340a3
Eric Villnow <eric@ericvillnow.com>
2015-07-08 02:27:07 +0000

    Finished Bootstrap on index.php

--- index.php
+++ index.php
@@ -11,20 +11,29 @@
     <title><?php echo $title; ?></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link rel="stylesheet" href="timeclock.css" >
+	<style type="text/css">
+        .error {
+            color: #ff0000;
+        }
+    </style>
   </head>
   <body>
     <div class="container">
-      <form class="form-signin">
+      <div class="alert alert-danger alert-dismissible" role="alert">Important: Log hours in 24hr format! Example: 3:00 PM translates to 15:00 and 9:00 AM translates to 09:00</div>
+      <form class="form-signin" method="post">
         <h2 class="form-signin-heading"><?php echo $title; ?></h2>
-        <input type="text" id="inputName" class="form-control form-control-top" placeholder="Name" required autofocus>
-        <input type="password" id="inputPassword" class="form-control form-control-mid" placeholder="password" required>
-		<input type="text" id="inputTimeIn" class="form-control form-control-bottom" placeholder="Time In" required>
+        <p>Looking to check your hours? Click <a href="getloghours.php">here</a></p>
+        <p><span class="error">* required field.</span></p>
+        <input type="text" name="name" id="inputName" class="form-control form-control-top" placeholder="Name" required autofocus>
+        <!--<input type="password" name="password" id="inputPassword" class="form-control form-control-mid" placeholder="password">-->
+		<input type="text" name="time_in" id="inputTimeIn" class="form-control form-control-bottom" placeholder="Time In" required>
+		<input type="text" name="time_out" id="inputTimeIn" class="form-control form-control-bottom" placeholder="Time Out" required>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
           </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
       </form>
     </div> <!-- /container -->
  
@@ -32,67 +41,30 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
 </html>
-<!--
-<html>
-    <head>
-        <title><?php echo $title; ?></title>
-        <style type="text/css">
-            .error {
-                color: #ff0000;
-            }
-        </style>
-    </head>
-    <body>
-        <h2>Robotics Team Sign-In</h2>
-        <p>Looking to check your hours? Click <a href="getloghours.php">here</a></p>
-        <h3 style="color:#FF0000;">Important: Log hours in 24hr format!<br/>
-        Example: 3:00 PM translates to 15:00 and <br/>9:00 AM translates to 09:00</h3>
-        <p><span class="error">* required field.</span></p>
-        <form method="post" action="index.php">
-        <table>
-            <tr>
-                <td>Name:</td>
-                <td><input type="text" name="name" required /><span class="error">* </span></td>
-            </tr>
-            <tr>
-                <td>Time In:</td>
-                <td><input type="text" name="time_in" required /><span class="error">* </span></td>
-            </tr>
-            <tr></tr>   
-                <td>Time Out:</td>
-                <td><input type="text" name="time_out" required /><span class="error">* </span></td>
-            </tr>
-            <tr>
-                <td><input type="submit"/></td>
-            </tr>
-        </table>
-        <?php 
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $database);
-        
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        
-        if(isset($_POST['name'])) {
-            $name = $_POST['name'];
-            $time_in = $_POST['time_in'];
-            $time_out = $_POST['time_out'];
-            $date = date('Y-m-d');
-            
-            $sql = "INSERT INTO `hours` (`User`, `Date`, `Time_In`, `Time_Out`) VALUES ('" . $name . "', '" . $date . "', '" . $time_in . "', '" . $time_out . "')";
-            
-            $conn->query($sql);
-            
-            echo "<h2>Hours logged:</h2><br>";
-            echo $name . "<br>";
-            echo $time_in . "<br>";
-            echo $time_out;
-        }
 
+<?php 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-        
-        ?>
-    </body>
-</html>-->
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if(isset($_POST['name'])) {
+    $name = $_POST['name'];
+    $time_in = $_POST['time_in'];
+    $time_out = $_POST['time_out'];
+    $date = date('Y-m-d');
+    
+    $sql = "INSERT INTO `hours` (`User`, `Date`, `Time_In`, `Time_Out`) VALUES ('" . $name . "', '" . $date . "', '" . $time_in . "', '" . $time_out . "')";
+    
+    $conn->query($sql);
+    
+    echo "<h2>Hours logged:</h2><br>";
+    echo $name . "<br>";
+    echo $time_in . "<br>";
+    echo $time_out;
+}
+
+?>
